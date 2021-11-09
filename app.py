f132f972c10e136b073a9045d2200e17d5b02fb1
sakamoto-ryusei02 <91442126+sakamoto-ryusei02@users.noreply.github.com>
2021-11-09 10:20:48 +0900

    dbcom

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from oseti import oseti
 from flask import Flask, render_template, url_for, request, redirect
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
 #スコアの取得
 def getScore(list):
 
@@ -33,6 +34,48 @@ def getAnalyzer(txt):
     return analyzer.count_polarity(txt)
 
 app = Flask(__name__)
+
+# Flaskの立ち上げ
+app.config['SECRET_KEY'] = 'secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.sqlite'  # DBへのパス
+
+# SQLAlchemyでデータベース定義
+
+db = SQLAlchemy(app)
+
+# SQLiteのDBテーブル情報
+class FLASKDB(db.Model):
+    __tablename__ = 'flask_table'
+
+    ID = db.Column(Integer, primary_key=True)
+    YOURNAME = db.Column(String(32))
+    AGE = db.Column(Integer)
+
+# DBの作成
+db.create_all()
+    
+
+# 127.0.0.1/DBINFO:5000に遷移したときの処理
+@app.route('/DBINFO', methods=['POST', 'GET'])
+def bokinbox():
+    if request.method == 'POST':
+        yourname = request.form['yourname']
+        age = request.form['age']
+        flask = FLASKDB(YOURNAME=yourname, AGE=age)
+        db.session.add(flask)
+        db.session.commit()
+        db.session.close()
+        FLASKDB_infos = db.session.query(
+            FLASKDB.ID, FLASKDB.YOURNAME, FLASKDB.AGE).all()
+        return render_template('db_info.html', FLASKDB_infos=FLASKDB_infos)
+
+# 127.0.0.1/DBINFO:5000に遷移したときの処理
+@app.route('/search', methods=['GET'])
+def search():
+        user = db.session.query(FLASKDB.YOURNAME).filter(FLASKDB.AGE == 20).all()
+
+        return render_template('search.html', user=user)
+
 
 @app.route('/', methods=['GET'])
 def index():
